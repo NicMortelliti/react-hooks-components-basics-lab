6b7e15efc38bce5940b2fdbabc113b9ae6fcdf3c
NicMortelliti <npmortelliti@gmail.com>
2022-03-19 15:17:05 -0700

    commit tree 0d3d3f0326f377fd6a49e4be00b93b5463716e12

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,14 @@
 import React from "react";
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
-
+import NavBar from "./NavBar.js";
+import Home from "./Home.js";
+import About from "./About.js"
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
